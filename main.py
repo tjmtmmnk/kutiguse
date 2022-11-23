8cda169b9566adaa73ed5888d5899586a0e58f65
momochi <tjmtmmnk@gmail.com>
2022-11-24 08:19:04 +0900

    複数の文章から候補となるngramを列挙する

---
+++ main.py
@@ -0,0 +1,41 @@
+from janome.tokenizer import Tokenizer, Token
+from collections import defaultdict
+
+tokenizer = Tokenizer()
+
+sentences = [
+    "変な話今日は雨が降りそう",
+    "変な人"
+]
+
+MAX_NGRAM_N = 5
+
+
+def ngram(tokens: list[Token], n: int) -> list[str]:
+    grams = []
+    for i in range(len(tokens)):
+        gram = ""
+        for j in range(n):
+            idx = i + j
+            if idx < len(tokens):
+                gram += tokens[idx].surface
+        grams.append(gram)
+    return grams
+
+
+def enumerate_ngram_candidates(input_texts: list[str]) -> list[str]:
+    ngram_to_text_set = defaultdict(lambda: set())
+    for i, text in enumerate(input_texts):
+        tokens = list(tokenizer.tokenize(text))
+        for j in range(1, MAX_NGRAM_N):
+            jgram = ngram(tokens, j)
+            for jg in jgram:
+                ngram_to_text_set[jg].add(i)  # 同一文章では同じトークンを最大1回しかカウントしないように制限する
+    ngram_candidates = []
+    for g, texts in ngram_to_text_set.items():
+        if len(texts) > 1:
+            ngram_candidates.append(g)
+    return ngram_candidates
+
+
+print(enumerate_ngram_candidates(sentences))
